62645ba1a0d272afde40f30e74ae9e23831abc12
jhmatthes <jaclyn.hatala.matthes@gmail.com>
2016-06-24 13:28:07 -0400

    new workflow organization

---
+++ lab_moisture_soilflux_workflow.R
@@ -0,0 +1,11 @@
+# This file outlines the workflow for the Lab Soil Flux Moisture Manipulation Experiment
+# Summer 2016, Jaclyn Hatala Matthes
+
+#load libraries
+library(chron)
+library(ggplot2)
+library(plyr)
+lapply(list.files(path="R/",pattern = "[.]R$", recursive = TRUE), source)
+
+
+
